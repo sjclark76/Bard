1529dddba0849be1bf4f19b077d17f3d36c2fde2
Stuart Clark <sjclark76@gmail.com>
2020-08-01 09:25:42 +1200

    Add in GRPC service

---
+++ src/Bard.Tests/gRPC/gRPCTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Bard.gRPCService;
+using Grpc.Net.Client;
+using Xunit;
+
+namespace Fluent.Testing.Library.Tests.gRPC
+{
+    public class gRPCTests
+    {
+        [Fact]
+        public async Task Foo()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client =  new CreditRatingCheck.CreditRatingCheckClient(channel);
+            var creditRequest = new CreditRequest { CustomerId = "id0201", Credit = 7000};
+            var reply = await client.CheckCreditRequestAsync(creditRequest);
+
+        }
+    }
+}

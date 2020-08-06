98e22392fca2be2acc8986a459ec57fe355f8238
Stuart Clark <sjclark76@gmail.com>
2020-08-06 18:39:45 +1200

    more xml comments

--- src/Bard/IChapterGiven.cs
+++ src/Bard/IChapterGiven.cs
@@ -2,8 +2,18 @@
 
 namespace Bard
 {
+    /// <summary>
+    /// Interface to help the fluent interface story builder
+    /// </summary>
+    /// <typeparam name="TStoryData">The StoryData</typeparam>
+    /// <typeparam name="TStoryParams">The Story Parameters</typeparam>
     public interface IChapterGiven<TStoryData, out TStoryParams> where TStoryData : class, new() where TStoryParams : new()
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <returns></returns>
         IChapterGivenWhen<TStoryData> When(Action<ScenarioContext<TStoryData>, TStoryParams> execute);
     }
 }

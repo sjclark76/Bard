76488609320713dc89e81450deaf1f0e8a7df64c
Stuart Clark <stuart.clark@education.govt.nz>
2020-11-16 08:57:41 +1300

    fix build warnings

--- src/Bard/Internal/Then/PerformanceMonitor.cs
+++ src/Bard/Internal/Then/PerformanceMonitor.cs
@@ -16,38 +16,39 @@ namespace Bard.Internal.Then
         public void AssertElapsedTime(Func<IResponse>? apiRequest, ApiResult apiResult, int? maxElapsedTime)
         {
             const int retryCount = 3;
-            IResponse RetryApiCall(int i)
+
+            IResponse RetryApiCall(Func<IResponse> api, int i)
             {
-                var response = apiRequest();
-                
-                _logWriter.LogMessage($"Retry #{i + 1} Response Time: {response.ElapsedTime?.TotalMilliseconds} (milliseconds)");
+                var response = api();
+
+                _logWriter.LogMessage(
+                    $"Retry #{i + 1} Response Time: {response.ElapsedTime?.TotalMilliseconds} (milliseconds)");
                 _logWriter.BlankLine();
                 _logWriter.LogLineBreak();
                 _logWriter.BlankLine();
                 return response;
             }
 
-            if (apiResult.ExceededElapsedTime(maxElapsedTime))
+            if (apiRequest == null || apiResult.ExceededElapsedTime(maxElapsedTime) == false)
+                return;
+
+            _logWriter.LogHeaderMessage(
+                $"The API response took longer than {maxElapsedTime} milliseconds. ({apiResult.ElapsedTime?.TotalMilliseconds})");
+
+            var totalTime = new TimeSpan();
+
+            for (var i = 0; i < retryCount; i++)
             {
-                if (apiRequest == null) return;
-                
-                _logWriter.LogHeaderMessage($"The API response took longer than {maxElapsedTime} milliseconds. ({apiResult.ElapsedTime?.TotalMilliseconds})");
-                
-                var totalTime = new TimeSpan();
-                
-                for (var i = 0; i < retryCount; i++)
-                {
-                    var response = RetryApiCall(i);
+                var response = RetryApiCall(apiRequest, i);
 
-                    totalTime = totalTime.Add(response.ElapsedTime.GetValueOrDefault());
-                }
-
-                var averageTime = totalTime.Divide(retryCount);
-                
-                _logWriter.LogMessage($"Average Response Time: {averageTime.TotalMilliseconds} (milliseconds)");
-                
-                apiResult.AssertElapsedTime(averageTime, maxElapsedTime);
+                totalTime = totalTime.Add(response.ElapsedTime.GetValueOrDefault());
             }
+
+            var averageTime = totalTime.Divide(retryCount);
+
+            _logWriter.LogMessage($"Average Response Time: {averageTime.TotalMilliseconds} (milliseconds)");
+
+            apiResult.AssertElapsedTime(averageTime, maxElapsedTime);
         }
     }
 }
